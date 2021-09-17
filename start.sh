bc8a21ca04a6968beb0926ac88c3d74e0eb6dc8c
David Mauskop <david@render.com>
2021-09-17 00:39:59 -0700

    Maintain correct permissions for /home/dev

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@
 mkdir -p /home/dev/.ssh
 cp /etc/secrets/key.pub /home/dev/.ssh/authorized_keys
 
+# Maintain correct permissions on redeploy
+# dropbear complains if /home/dev is "writable by others"
+chmod -R g-w /home/dev
+
 dropbear -F -E -k -s -w
