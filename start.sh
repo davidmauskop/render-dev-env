af24eb28f31494d5a4b295b9ed9e7c97de418735
David Mauskop <david@render.com>
2021-09-21 22:34:53 -0700

    Use a symlink

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /home/dev/.ssh
-cp /etc/secrets/key.pub /home/dev/.ssh/authorized_keys
+ln -sf /etc/secrets/key.pub /home/dev/.ssh/authorized_keys
 
 # Maintain correct permissions on redeploy
 # dropbear complains if /home/dev is "writable by others"
